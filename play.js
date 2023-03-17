ba52bc3e5e3b17a6ba75d2496b8f2f021cdb1066
= <d-weatherford@live.com>
2023-03-17 12:30:36 -0600

    Finished login.js and fleshed out all of play.js except for Game.

---
+++ play.js
@@ -0,0 +1,100 @@
+const btnDescriptions = [
+    { file: 'sound1.mp3', hue: 120 },
+    { file: 'sound2.mp3', hue: 0 },
+    { file: 'sound3.mp3', hue: 60 },
+    { file: 'sound4.mp3', hue: 240 },
+]
+
+class Button {
+    constructor(desc, el) {
+        this.el = el;
+        this.hue = desc.hue;
+        this.sound = loadSound(desc.file);
+        this.paint(25);
+    }
+
+    paint(level) {
+        const bg = `hsl(${this.hue}), 100%, ${level}%`;
+        this.el.style.backgroundColor = bg;
+    }
+
+    async press(volume) {
+        this.paint(50);
+        await this.play(volume);
+        this.paint(25);
+    }
+
+    async play(volume = 1.0) {
+        this.sound.volume = volume;
+        await new Promise((resolve) => {
+            this.sound.onended = resolve;
+            this.sound.play();
+        });
+    }
+}
+
+class Game {
+    buttons;
+    allowPlayer;
+    sequence;
+    playerPlaybackPos;
+    mistakeSound;
+
+    constructor() {
+
+    }
+
+    async pressButton(button) {
+
+    }
+
+    async reset() {
+
+    }
+
+    getPlayerName() {
+
+    }
+
+    async playSequence() {
+
+    }
+
+    addButton() {
+
+    }
+
+    updateScore(score) {
+
+    }
+
+    async buttonDance(laps = 1) {
+
+    }
+
+    getRandomButton() {
+
+    }
+
+    saveScore(score) {
+
+    }
+
+    updateScores(userName, score, scores) {
+
+    }
+}
+
+const game = new Game();
+
+function delay(ms) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(true);
+        }, ms);
+    });
+}
+
+function loadSound(filename) {
+    return new Audio('assets/' + filename);
+}
