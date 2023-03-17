392b80229c4cc66792f76790d1ef86370297cbc2
= <d-weatherford@live.com>
2023-03-17 12:38:12 -0600

    Button pressing works.

--- play.js
+++ play.js
@@ -41,11 +41,24 @@ class Game {
     mistakeSound;
 
     constructor() {
+        this.buttons = new Map();
+        this.allowPlayer = false;
+        this.sequence = [];
+        this.playerPlaybackPos = 0;
+        this.mistakeSound = loadSound('error.mp3');
 
+        document.querySelectorAll('.game-button').forEach((el, i) => {
+            if (i < btnDescriptions.length) {
+                this.buttons.set(el.id, new Button(btnDescriptions[i], el));
+            }
+        });
+
+        const playerNameEl = document.querySelector('.player-name');
+        playerNameEl.textContent = this.getPlayerName();
     }
 
     async pressButton(button) {
-
+        console.log(button.id);
     }
 
     async reset() {
